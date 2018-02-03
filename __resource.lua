91b4a35f831b17aa02e9a8eea2899dee3e5ddb04
Flatracer <flatracer@ymail.com>
2018-02-03 13:12:43 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,8 +3,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Discord Bot' 			-- Resource Description
 
 server_script {						-- Server Scripts
-	'server/server.lua',
 	'config.lua',
+	'server/server.lua',
 }
 client_script 'client/client.lua'	-- Client Script
 
